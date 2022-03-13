28792ab9ff0c2721a5011d3c00ac767dbb4d9496
BarracudaPff <mrm.kikll@gmail.com>
2022-03-13 11:33:13 +0300

    upd 6

--- app/src/main/java/com/samsung/chatexample/activities/DialogActivity.java
+++ app/src/main/java/com/samsung/chatexample/activities/DialogActivity.java
@@ -1,16 +1,25 @@
 package com.samsung.chatexample.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.samsung.chatexample.R;
 
 public class DialogActivity extends AppCompatActivity {
+    RecyclerView chatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+
+        initViews();
+
+    }
+
+    public void initViews() {
+        chatView = findViewById()
     }
 }
